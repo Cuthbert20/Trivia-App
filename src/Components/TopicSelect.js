97b6455f10f05d1838c84a809e946719efb2c08e
Cuthbert20 <knowltonspencer@gmail.com>
2019-10-15 14:57:42 -0600

    first commit Triva app layout kalam font

---
+++ src/Components/TopicSelect.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./styles/Main.scss";
+
+export default class TopicSelect extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Select Your Topic</h1>
+      </div>
+    );
+  }
+}
