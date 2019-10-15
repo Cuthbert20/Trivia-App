4f41ef2d04413fa26a06ecf97a53382344144c7b
Cuthbert20 <knowltonspencer@gmail.com>
2019-10-15 15:39:33 -0600

    setting up some basic styling and layout

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import TopicSelect from "./Components/TopicSelect";
 
 function App() {
   return (
-    <div>
-      <h1>Time For Trivia</h1>
+    <div className="">
+      <h1>It's That Time Again, are You Ready to Play?!?!</h1>
+      {/* PlayTime Component use hooks with button if yes turn background color to Crimson */}
       <TopicSelect />
     </div>
   );
--- src/Components/TopicSelect.js
+++ src/Components/TopicSelect.js
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
 import "./styles/Main.scss";
+import pugSwing from "../media/marion-michele-z_eFLP9aS6s-unsplash.jpg";
 
 export default class TopicSelect extends Component {
   render() {
     return (
-      <div>
+      <div className="select-container">
         <h1>Select Your Topic</h1>
+        <section>
+          <img
+            className="fun-img"
+            src={pugSwing}
+            alt="Pug in a Swing Smiling"
+          />
+        </section>
       </div>
     );
   }
