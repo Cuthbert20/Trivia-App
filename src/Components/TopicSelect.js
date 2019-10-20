8a28284c6771aca03785bb273bea86a2cabad14d
Cuthbert20 <knowltonspencer@gmail.com>
2019-10-20 17:05:25 -0600

    changed topicselect component to function using react-spring for animation

--- src/Components/TopicSelect.js
+++ src/Components/TopicSelect.js
@@ -1,20 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 import "./styles/Main.scss";
 import pugSwing from "../media/marion-michele-z_eFLP9aS6s-unsplash.jpg";
+import { useSpring, animated } from "react-spring";
 
-export default class TopicSelect extends Component {
-  render() {
-    return (
-      <div className="select-container">
-        <h1>Select Your Topic</h1>
-        <section>
-          <img
-            className="fun-img"
-            src={pugSwing}
-            alt="Pug in a Swing Smiling"
-          />
-        </section>
-      </div>
-    );
-  }
+function TopicSelect() {
+  const props = useSpring({ opacity: 1, from: { opacity: 0 } });
+  return (
+    <div className="select-container">
+      <h1>Select Your Topic</h1>
+      <section></section>
+      <animated.div style={props}>
+        <p>I will fade in</p>
+        <img className="fun-img" src={pugSwing} alt="Pug in a Swing Smiling" />
+      </animated.div>
+    </div>
+  );
 }
+
+export default TopicSelect;
