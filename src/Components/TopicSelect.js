08e60f154a68bd238510bad2a422b84957132551
Cuthbert20 <knowltonspencer@gmail.com>
2019-10-21 12:09:45 -0600

    fetching trivia data and using react-hook setCategory to assign value to category

--- src/Components/TopicSelect.js
+++ src/Components/TopicSelect.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./styles/Main.scss";
 import pugSwing from "../media/marion-michele-z_eFLP9aS6s-unsplash.jpg";
 import { useSpring, animated } from "react-spring";
+import axios from "axios";
 
 function TopicSelect() {
   const props = useSpring({ opacity: 1, from: { opacity: 0 } });
+  const [category, setCategory] = useState({ data: [] });
+  useEffect(() => {
+    async function fetchData() {
+      const result = await axios.get(`https://opentdb.com/api_category.php`);
+      // console.log(result.data.trivia_categories);
+      setCategory({
+        data: result.data.trivia_categories
+      });
+    }
+    fetchData();
+  });
   return (
     <div className="select-container">
       <h1>Select Your Topic</h1>
