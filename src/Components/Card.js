4a7fcc9c7d3974cf71ebb4e518de3041362df4fb
Cuthbert20 <knowltonspencer@gmail.com>
2019-10-22 16:23:10 -0600

    end of day push, added styling, sweetalert2 for answers

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
 import "./styles/Main.scss";
 import axios from "axios";
+import Swal from "sweetalert2";
 
 function Card(props) {
   const [category, setCategory] = useState("");
   const [questions, setQuestions] = useState([]);
+  //state hook for modal initialState false
+  const [show, setShow] = useState(false);
   const { topic } = props;
   //   console.log(topic);
   useEffect(() => {
@@ -30,12 +33,27 @@ function Card(props) {
   };
   let topicQuestions = async () => {
     let result = await axios.get(
-      `https://opentdb.com/api.php?amount=10&category=${topic}&encode=url3986`
+      //when fetching data from Trivia API we are get topic from props.topic, multiple choice & encoded with url3986
+      `https://opentdb.com/api.php?amount=1&category=${topic}&type=multiple&encode=url3986`
     );
     setQuestions(result.data.results);
     // if (topic !== "") {
     //   setQuestions(result.data.results);
     // }
+  };
+  //   let showModal = () => {
+  //     setShow(!show);
+  //   };
+  //   let hideModal = () => {
+  //     setShow(false);
+  //   };
+  let handleAnswer = val => {
+    Swal.fire({
+      title: "Answer",
+      text: val.target.value,
+      //   not able to add a class to sweetalert
+      customClass: "swal2-popup"
+    });
   };
   console.log(category);
   console.log(questions);
@@ -54,10 +72,18 @@ function Card(props) {
       </h3>
       <button onClick={() => topicQuestions()}>Questions!!</button>
       {questions.map((elm, index) => {
+        console.log(elm);
         return (
-          <div key={index}>
+          <div className="Card-question" key={index}>
             {/* Using decodeURIComponent to decode questions that are return with URL Encoding (RFC 3986) */}
             <h2>{decodeURIComponent(elm.question)}</h2>
+            <h3>{elm.difficulty}</h3>
+            <button
+              value={decodeURIComponent(elm.correct_answer)}
+              onClick={e => handleAnswer(e)}
+            >
+              See Answer
+            </button>
           </div>
         );
       })}
