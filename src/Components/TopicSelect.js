6e66fdaecc672b63e7660580e9291bbe80ef6360
Cuthbert20 <knowltonspencer@gmail.com>
2019-10-22 10:45:39 -0600

    set value of category to an array and am mapping over it

--- src/Components/TopicSelect.js
+++ src/Components/TopicSelect.js
@@ -6,21 +6,45 @@ import axios from "axios";
 
 function TopicSelect() {
   const props = useSpring({ opacity: 1, from: { opacity: 0 } });
-  const [category, setCategory] = useState({ data: [] });
+  const [category, setCategory] = useState([]);
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const result = await axios.get(`https://opentdb.com/api_category.php`);
+  //     // console.log(result.data.trivia_categories);
+  //     setCategory(
+  //       {
+  //         category: result.data.trivia_categories
+  //       },
+  //       []
+  //     );
+  //   }
+  // });
   useEffect(() => {
-    async function fetchData() {
-      const result = await axios.get(`https://opentdb.com/api_category.php`);
-      // console.log(result.data.trivia_categories);
-      setCategory({
-        data: result.data.trivia_categories
-      });
-    }
-    fetchData();
-  });
+    topics();
+    // axios.get(`https://opentdb.com/api_category.php`).then(res => {
+    //   setCategory({
+    //     category: res.data.trivia_categories
+    //   });
+    // });
+  }, []);
+  let topics = () => {
+    axios.get(`https://opentdb.com/api_category.php`).then(res => {
+      //setting category to the value of the returned promise from the data we fetched from the trivia api
+      setCategory(res.data.trivia_categories);
+      // console.log(res.data.trivia_categories);
+    });
+  };
+  // fetchData();
+  console.log(category.length);
+  console.log(category);
   return (
     <div className="select-container">
       <h1>Select Your Topic</h1>
-      <section></section>
+      <ul>
+        {category.map((elm, index) => {
+          return <li key={elm.id}>{elm.name}</li>;
+        })}
+      </ul>
       <animated.div style={props}>
         <p>I will fade in</p>
         <img className="fun-img" src={pugSwing} alt="Pug in a Swing Smiling" />
